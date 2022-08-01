5833381955ad8eeb362370bd26857ebe0a00c0e0
Andrii Davydov <endry.davidko666@gmail.com>
2022-08-01 17:31:11 +0300

    edit oleg JS

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-"use strict";
-let storeName = "Shoes Store";
+'use strict';
+let storeName = 'Shoes Store';
 let storeDescription = {
-    budget: 10000,
-    employees: ["Ira", "Tanya", "Ihor"],
-    products: {
-        'product 1': 1000,
-        'product 2': 2000,
-    },
-    open: true
+	budget: 10000,
+	employees: ['Oleg', 'Andrii', 'Stepan'],
+	products: {
+		'product 1': 1000,
+		'product 2': 2000,
+	},
+	open: true,
 };
 storeDescription.products['product 3'] = 3000;
 //console.log(storeDescription.products);
 
 let a = 10;
-let b = "10";
+let b = '10';
 console.log(a !== b);
 
-let bc = "new variable1git";
+let bc = 'new variable1git';
 let asfd = 1;
 let as = 4;

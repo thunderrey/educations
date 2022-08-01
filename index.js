5e221b43f5b61396f1c1af3e46977a903d9aedb2
Oleg <onlinebuisnes@gmail.com>
2022-08-01 16:28:34 +0300

    new variable index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+let storeName = "Shoes Store";
+let storeDescription = {
+    budget: 10000,
+    employees: ["Ira", "Tanya", "Ihor"],
+    products: {
+        'product 1': 1000,
+        'product 2': 2000,
+    },
+    open: true
+};
+storeDescription.products['product 3'] = 3000;
+//console.log(storeDescription.products);
+
+let a = 10;
+let b = "10";
+console.log(a !== b);
+
+let bc = "new variable";
